f059d2e0a5125a8bde3e57915ad39a71ccb0da5c
Breania Harvey <harvey.breania@gmail.com>
2021-10-19 23:00:07 -0500

    DELETE request not functioning properly

---
+++ server.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const express = require('express');
+const savedNotes = require('./Develop/db/db.json');
+const fs = require('fs');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('./Develop/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body
+    newNote.id = savedNotes.length.toString();
+
+    savedNotes.push(newNote);
+    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(savedNotes));
+
+    res.json(savedNotes);
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(savedNotes);
+});
+
+app.delete('/api/notes:id', (req, res) => {
+    const { id } = req.params;
+
+    const newSavedNotes = savedNotes.filter(note => {
+        if (note.id !== id) {
+            return note;
+        }
+        return newSavedNotes;
+    });
+
+    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(newSavedNotes));
+
+    return res.send();
+});
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
